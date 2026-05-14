8ca6365a3b69668a871adf753927c30c470653ad
thepetershen <thepetershen@gmail.com>
2026-05-13 23:54:13 -0700

    Discovered that no good kaggle datasets exists for this project. Pivoted to using selenium because of website disabling broad scraping.

--- scraper.py
+++ scraper.py
@@ -2,8 +2,15 @@ import os
 import psycopg2
 import pandas as pd
 from dotenv import load_dotenv
+import time
 
-load_dotenv()
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+
+url = "https://bwfworldtour.bwfbadminton.com/calendar/?cyear=2025&rstate=all"
+driver = webdriver.Chrome()
+driver.get(url)
+time.sleep(5)
 
 conn = psycopg2.connect(
     host="localhost",
@@ -11,3 +18,6 @@ conn = psycopg2.connect(
     user="petershen",
     port="5432"
 )
+
+
+
