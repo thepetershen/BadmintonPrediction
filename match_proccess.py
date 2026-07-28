2d1df671cc6937d1def827df2ce1d2995d636681
thepetershen <thepetershen@gmail.com>
2026-07-28 09:39:47 -0700

    code to reformat all tournaments into 2 csv files.

--- match_proccess.py
+++ match_proccess.py
@@ -1,14 +1,11 @@
 import pandas as pd
-
 from dataclasses import dataclass
 from typing import Optional
 from datetime import date
-
 import time
-
+from dataclasses import asdict
 import undetected_chromedriver as uc
 from selenium.webdriver.common.by import By
-
 from curl_cffi import requests
 
 # we will aim to create 2 files. 1 where names are proccessed and one where they are not. (for publishing vs our use)
@@ -108,7 +105,6 @@ params = {
 }
 
 
-
 def cookie_check():
   try:
     time.sleep(8)
@@ -198,7 +194,7 @@ def reformat_tournament(file_path):
 
       full_name_2 = first_name + " " + last_name
 
-      id_to_name[player_id_1] = full_name_2
+      id_to_name[player_id_2] = full_name_2
 
     if player_id_1 == row["winner_id"]:
       winner_name = full_name_1
@@ -219,23 +215,88 @@ def reformat_tournament(file_path):
           rankings[player_id_2] = (player_2_cur_rank, player_2_highest_rank)
           
     else: 
-        # else reset all the data we have
-        rankings = {}
-        tracked_week = tournament_week
-        tracked_year = tournament_year
-        
-        player_1_cur_rank, player_1_highest_rank = get_rank(player_id_1, tournament_year, tournament_week)
-        rankings[player_id_1] = (player_1_cur_rank, player_1_highest_rank)
-        
-        player_2_cur_rank, player_2_highest_rank = get_rank(player_id_2, tournament_year, tournament_week)
-        rankings[player_id_2] = (player_2_cur_rank, player_2_highest_rank)
+      # else reset all the data we have
+      rankings = {}
+      tracked_week = tournament_week
+      tracked_year = tournament_year
+      
+      player_1_cur_rank, player_1_highest_rank = get_rank(player_id_1, tournament_year, tournament_week)
+      rankings[player_id_1] = (player_1_cur_rank, player_1_highest_rank)
+      
+      player_2_cur_rank, player_2_highest_rank = get_rank(player_id_2, tournament_year, tournament_week)
+      rankings[player_id_2] = (player_2_cur_rank, player_2_highest_rank)
 
-    print (player_1_cur_rank, player_2_cur_rank, player_1_highest_rank, player_2_highest_rank)
-    
+    new_published_match = MatchRecordPublish(
+      tournament_name=row['tournament_name'],
+      tournament_level=row['tournament_level'],
+      match_date=row['match_date'],
+      round_name=row['round_name'],
+      
+      g1_p1_score=row['g1_p1_score'],
+      g1_p2_score=row['g1_p2_score'],
+      g2_p1_score=row.get('g2_p1_score'), 
+      g2_p2_score=row.get('g2_p2_score'),
+      g3_p1_score=row.get('g3_p1_score'),
+      g3_p2_score=row.get('g3_p2_score'),
+
+     
+      player_1_name=full_name_1,               
+      player_2_name=full_name_2,               
+      winner_name=winner_name,            
+      
+      player_1_rank=player_1_cur_rank,        
+      player_2_rank=player_2_cur_rank,        
+      player_1_rank_highest=player_1_highest_rank, 
+      player_2_rank_highest=player_2_highest_rank
+    )
+
+    new_match = MatchRecord(
+      tournament_name=row['tournament_name'],
+      tournament_level=row['tournament_level'],
+      match_date=row['match_date'],
+      round_name=row['round_name'],
+      
+      g1_p1_score=row['g1_p1_score'],
+      g1_p2_score=row['g1_p2_score'],
+      g2_p1_score=row.get('g2_p1_score'), 
+      g2_p2_score=row.get('g2_p2_score'),
+      g3_p1_score=row.get('g3_p1_score'),
+      g3_p2_score=row.get('g3_p2_score'),
+
+      
+      player_1_id=player_id_1,               
+      player_2_id=player_id_2,               
+      winner_id=row['winner_id'],            
+      
+      player_1_rank=player_1_cur_rank,        
+      player_2_rank=player_2_cur_rank,        
+      player_1_rank_highest=player_1_highest_rank, 
+      player_2_rank_highest=player_2_highest_rank
+    )
+
+    all_matches.append(new_match)
+    all_matches_publish.append(new_published_match)
+  
 
   return
 
-reformat_tournament("data/rawtournament/2022-gwangju-yonex-korea-masters_matchdata.csv")
+df_tournaments = pd.read_csv("data/all_tournaments.csv")
+
+for index, row in df_tournaments.iterrows():
+  tournament_link = row['tournament link']
+  clean_name_from_link = tournament_link.split('/')[5]
+  tournament_path = "data/rawtournament/" + clean_name_from_link + "_matchdata.csv"
+  reformat_tournament(tournament_path)
+
+
+all_match_dicts = [asdict(match) for match in all_matches]
+all_match_publish_dicts = [asdict(match) for match in all_matches_publish]
+
+df_matches = pd.DataFrame(all_match_dicts) # directly convert dictionary to dataframe
+df_matches_publish = pd.DataFrame(all_match_publish_dicts)
+df_matches.to_csv("data/all_match_id.csv", index=False)
+df_matches_publish.to_csv("data/all_match_name.csv", index=False)
+
 
 
 driver.quit()
