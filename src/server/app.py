4f2a42ff0aa9801da16819639a4c9e1fd3bbf63f
thepetershen <thepetershen@gmail.com>
2026-07-31 11:57:29 -0700

    load in the dicts

--- src/server/app.py
+++ src/server/app.py
@@ -1,5 +1,5 @@
 import joblib
-
+import json
 from fastapi import FastAPI
 
 app = FastAPI()
@@ -10,9 +10,17 @@ model = joblib.load('data/models/xgb_badminton_model.joblib')
 # players they want us to scrape. we need to be able to find out using their name, their rank, and also there head to head
 # the head to head can easily be found.
 
+# load the name to id
+with open('data/name_to_id.json', 'r') as f:
+  name_to_id = {str(k): v for k, v in json.load(f).items()}
 
-app.get("/")
-def predict_winner():
-  return 
+with open('data/h2h.json', 'r') as f:
+  h2h = {str(k): v for k, v in json.load(f).items()}
+
+
+@app.get("/predict/matchup/{player1_id}/{player2_id}")
+def predict_match(player1_id: int, player2_id: int):
+    
+  return {"player1_id": player1_id, "player2_id": player2_id, "prediction": ...}
 
 
