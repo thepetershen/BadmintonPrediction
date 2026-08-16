d6e0e3c3ab4ed3288b05e4d5248f4bb39699e143
thepetershen <thepetershen@gmail.com>
2026-08-15 22:41:53 -0700

    working frontend

--- src/frontend/src/App.jsx
+++ src/frontend/src/App.jsx
@@ -1,15 +1,27 @@
 import SearchComponent from "./SearchComponent.jsx"
 import ExpectedWinnerComponent from "./ExpectedWinnerComponent.jsx"
 import {useState} from 'react'
+import './App.css'
 
 function App() {
   const [winnerInfo, setWinnerInfo] = useState({"player1_name": "", "player2_name": "", "prediction": 0 });
 
   return (
-    <>
+    <div className="app">
+      <div className="app-header">
+        <h1>
+          Badminton Match Result Predictor
+        </h1>
+        <p>
+          Hey! Welcome to Badminton Match Result Predictor. This site aggregated publicly availible badminton match data to train several models in order to predict the result of a badminton match.
+          It is trained upon matches from Super 300 and above events from 2022 - 2026 (post pandemic).
+          Try out the model!
+          This project is also open sourced! Check out more info in methodology.
+        </p>
+      </div>
       <SearchComponent setWinnerInfo = {setWinnerInfo}/>
       <ExpectedWinnerComponent winnerInfo = {winnerInfo}/>
-    </>
+    </div>
   )
 }
 
